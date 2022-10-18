05eb096feae144c395f46e78132a2eeba21e826a
volkan <16579273+mevolkan@users.noreply.github.com>
2022-10-18 20:04:54 +0300

    new audio player

--- dist/video.88db5c57.js
+++ dist/video.88db5c57.js
@@ -1,34 +1,70 @@
 //play audio
-if (document.getElementById("playbtn")) {
-    const url = "https://uksouth.streaming.broadcast.radio/nrg";
-    const audio = new Audio(url);
-    const playBtn = document.getElementById("playbtn");
-    const progressEl = document.querySelector("progress");
-    let mouseDownOnSlider = false;
-    audio.addEventListener("loadeddata", ()=>{
-        progressEl.value = 0;
-    });
-    audio.addEventListener("timeupdate", ()=>{
-        if (!mouseDownOnSlider) progressEl.value = audio.currentTime / audio.duration * 100;
-    });
-    audio.addEventListener("ended", ()=>{
-        playBtn.textContent = "▶️";
-    });
+const schedule = [
+    "./images/breakfast-club.png",
+    "./images/the-am-show.png",
+    "./images/nrg-transit.png",
+    "./images/the-circle.png",
+    "./images/default.png", 
+];
+let hour = new Date().getHours();
+window.onload = function() {
+    var playBtn = document.getElementById("play");
+    var audio = document.getElementById("audio");
+    var showImg = document.getElementById("show-img");
+    if (hour >= 6 && hour < 10) showImg.src = schedule[0];
+    else if (hour >= 11 && hour < 14) showImg.src = schedule[1];
+    else if (hour >= 15 && hour < 19) showImg.src = schedule[2];
+    else if (hour >= 20 && hour <= 23) showImg.src = schedule[3];
+    else {
+        showImg.src = schedule[4];
+        console.log(schedule[4]);
+    }
+    audio.crossOrigin = "anonymous";
     playBtn.addEventListener("click", ()=>{
         audio.paused ? audio.play() : audio.pause();
-        playBtn.textContent = audio.paused ? "▶️" : "⏸️";
+        playBtn.textContent = audio.paused ? "Paused" : "Playing";
     });
-    progressEl.addEventListener("change", ()=>{
-        const pct = progressEl.value / 100;
-        audio.currentTime = (audio.duration || 0) * pct;
-    });
-    progressEl.addEventListener("mousedown", ()=>{
-        mouseDownOnSlider = true;
-    });
-    progressEl.addEventListener("mouseup", ()=>{
-        mouseDownOnSlider = false;
-    });
-}
+    playBtn.onclick = ()=>{
+        audio.src = "https://uksouth.streaming.broadcast.radio/nrg";
+        audio.load();
+        audio.play();
+        var context = new AudioContext();
+        var src = context.createMediaElementSource(audio);
+        var analyser = context.createAnalyser();
+        var canvas = document.getElementById("canvas");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        var ctx = canvas.getContext("2d");
+        src.connect(analyser);
+        analyser.connect(context.destination);
+        analyser.fftSize = 512;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+        var WIDTH = canvas.width;
+        var HEIGHT = canvas.height;
+        var barWidth = WIDTH / bufferLength;
+        var barHeight;
+        var x = 0;
+        function renderFrame() {
+            requestAnimationFrame(renderFrame);
+            x = 0;
+            analyser.getByteFrequencyData(dataArray);
+            ctx.fillStyle = "#000";
+            ctx.fillRect(0, 0, WIDTH, HEIGHT);
+            for(var i = 0; i < bufferLength; i++){
+                barHeight = dataArray[i];
+                var r = barHeight + 25 * (i / bufferLength);
+                var g = 255 * (i / bufferLength);
+                var b = 50;
+                ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+                ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+                x += barWidth * 1.5;
+            }
+        }
+        audio.play();
+        renderFrame();
+    };
+};
 //dark mode
 var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
 var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
