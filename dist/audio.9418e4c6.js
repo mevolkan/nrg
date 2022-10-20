23c6524444ab488c0fc414446585b16135fdfdd1
volkan <16579273+mevolkan@users.noreply.github.com>
2022-10-20 12:03:54 +0300

    changes

--- dist/audio.9418e4c6.js
+++ dist/audio.9418e4c6.js
@@ -1,4 +1,4 @@
 if (localStorage.getItem("color-theme") === "dark" || !("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) document.documentElement.classList.add("dark");
 else document.documentElement.classList.remove("dark");
 
-//# sourceMappingURL=audio.9418e4c6.js.map
+//# sourceMappingURL=video.9418e4c6.js.map
